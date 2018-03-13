eefb2afb99f0924100f2825df348b71862d47177
Anna Royer <anna.royer@colorado.edu>
2018-03-13 08:59:56 -0600

    change transaction result datatype to citetext

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180313031355) do
+ActiveRecord::Schema.define(version: 20180313144202) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,7 +64,7 @@ ActiveRecord::Schema.define(version: 20180313031355) do
     t.bigint "invoice_id"
     t.string "credit_card_number"
     t.datetime "credit_card_expiration_date"
-    t.string "result"
+    t.citext "result"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["invoice_id"], name: "index_transactions_on_invoice_id"
