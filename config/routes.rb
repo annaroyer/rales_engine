484238786a1eabb13166dd98b90c32618ce3e933
katyjane8 <katy.jane8@gmail.com>
2018-03-13 13:32:01 -0600

    routes for all invoice record endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :invoices, only: [:index]
+      namespace :invoices do
+        get '/find', to: "search#show"
+        get '/find_all', to: "search#index"
+        get '/random', to: "random#show"
+      end
+
+      resources :invoices, only: [:index, :show] do
+      end
     end
   end
 end
