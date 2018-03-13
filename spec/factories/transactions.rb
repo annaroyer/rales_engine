97d2292ede64ea8ab109deb405a284c5dd3a82da
Anna Royer <anna.royer@colorado.edu>
2018-03-13 13:39:46 -0600

    add customers controller directory with random controller

--- spec/factories/transactions.rb
+++ spec/factories/transactions.rb
@@ -2,7 +2,6 @@ FactoryBot.define do
   factory :transaction do
     invoice
     credit_card_number 1
-    credit_card_expiration_date "2018-03-12 17:46:09"
     result "MyString"
     created_at "MyString"
     updated_at "MyString"
