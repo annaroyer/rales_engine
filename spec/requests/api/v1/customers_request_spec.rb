e186a7f53d0fad0e81d9469790c472799685ddeb
Anna Royer <anna.royer@colorado.edu>
2018-03-13 13:41:49 -0600

    add controller spec for customers index and show endpoints

---
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -0,0 +1,25 @@
+require 'rails_helper'
+
+describe 'Customers API' do
+  it 'sends a list of customers' do
+    create_list(:customer, 3)
+
+    get '/api/v1/customers.json'
+
+    customers = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(customers.count).to eq(3)
+  end
+
+  it 'can get one customer by id' do
+    id = create(:customer).id
+
+    get "/api/v1/customers/#{id}.json"
+
+    customer = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(customer["id"]).to eq(id)
+  end
+end
