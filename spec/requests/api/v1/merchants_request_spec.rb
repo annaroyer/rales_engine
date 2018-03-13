af106d6e3143ae384163e36616461b8ba1cf3db4
Anna Royer <anna.royer@colorado.edu>
2018-03-12 20:13:43 -0600

    add api endpoints for find merchant by name and find all merchants by name

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -22,4 +22,31 @@ describe 'Merchants API' do
     expect(response).to be_success
     expect(merchant["id"]).to eq(id)
   end
+
+  it 'finds a merchant by name' do
+    name = "Shroeder-Jerde"
+    create(:merchant, name: name)
+
+    get "/api/v1/merchants/find?name=#{name}"
+
+    merchant = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchant["name"]).to eq(name)
+  end
+
+  it 'finds a list of merchants by name' do
+    name = "Shroeder-Jerde"
+    create_list(:merchant, 3, name: name)
+
+    get "/api/v1/merchants/find_all?name=#{name}"
+
+    merchants = JSON.parse(response.body)
+
+    expect(response).to be_success
+    expect(merchants.count).to eq(3)
+    merchants.each do |merchant|
+      expect(merchant["name"]).to eq(name)
+    end
+  end
 end
