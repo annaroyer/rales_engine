b1a20d3136a5363bb2a3d9eef709661dee39348b
Anna Royer <anna.royer@colorado.edu>
2018-03-14 12:46:14 -0600

    add revenue method to merchant model

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,5 +1,13 @@
 class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
+  has_many :invoice_items, through: :invoices
   validates_presence_of :name
+
+  def revenue
+    invoice_items
+    .joins(:transactions)
+    .merge(Transaction.success)
+    .sum('unit_price * quantity')
+  end
 end
