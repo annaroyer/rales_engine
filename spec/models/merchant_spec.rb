b8be4f991e0c72b89ae273fe797dae34ed577709
katyjane8 <katy.jane8@gmail.com>
2018-03-14 11:30:38 -0600

    started bi but finishing relationships first

--- spec/models/merchant_spec.rb
+++ spec/models/merchant_spec.rb
@@ -4,4 +4,25 @@ describe Merchant, type: :model do
   context 'validations' do
     it { should validate_presence_of :name }
   end
+
+  context 'class methods' do
+    before :each do
+      @merchant1 = create(:merchant)
+      @merchant2 = create(:merchant)
+      @merchant3 = create(:merchant)
+      invoice1 = create(:invoice, merchant: @merchant1)
+      invoice2 = create(:invoice, merchant: @merchant2)
+      invoice3 = create(:invoice, merchant: @merchant3)
+      invoice4 = create(:invoice, merchant: @merchant3)
+      create(:invoice_item, unit_price: 4000, quantity: 2, invoice: invoice1)
+      create(:invoice_item, unit_price: 5000, quantity: 3, invoice: invoice3)
+    end
+
+    describe ".most_revenue" do
+      xit "should return merchants with the most revenue" do
+
+      expect(Merchant.most_revenue(2)).to eq([@merchant3, @merchant1])
+      end
+    end
+  end
 end
