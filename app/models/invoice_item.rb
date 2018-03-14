876cfc9f6987c5e365ab65962fac8a940a9fc537
katyjane8 <katy.jane8@gmail.com>
2018-03-14 11:29:50 -0600

    models edited for relationship status

--- app/models/invoice_item.rb
+++ app/models/invoice_item.rb
@@ -1,4 +1,8 @@
 class InvoiceItem < ApplicationRecord
   belongs_to :item
   belongs_to :invoice
+
+  def self.invoices_by_cost
+    group(:invoice_id).order("sum(quantity * unit_price)")
+  end
 end
--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,4 +2,12 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   validates_presence_of :name
+
+  def total_revenue
+    invoices.sum(:total_revenue)
+  end
+
+  def self.most_revenue(quantity)
+    order(:total_revenue).reverse_order.limit(quantity)
+  end
 end
