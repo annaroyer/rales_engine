9fb4ddbd7303d218725dbc5b30d8d2b2401a7a2d
Anna Royer <anna.royer@colorado.edu>
2018-03-14 15:01:14 -0600

    debug for spec harness

--- app/serializers/invoice_item_serializer.rb
+++ app/serializers/invoice_item_serializer.rb
@@ -1,3 +1,3 @@
 class InvoiceItemSerializer < ActiveModel::Serializer
-  attributes :id, :item_id, :invoice_id, :quantity, :unit_price
+  attributes :id, :item_id, :invoice_id, :quantity, :price
 end
