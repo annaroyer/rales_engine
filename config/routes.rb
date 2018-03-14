9790e22d634874fe494a016d5face90d0f8348b9
katyjane8 <katy.jane8@gmail.com>
2018-03-14 17:16:19 -0600

    add routes for BI

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,8 @@ Rails.application.routes.draw do
         get 'random', to: 'random#index'
         get '/:id/items', to: 'items#index'
         get '/:id/invoices', to: 'invoices#index'
-        get '/most_revenue', to: 'revenue#index'
+        get '/most_revenue', to: 'most_revenue#index'
+        get '/most_items', to: 'most_items#index'
       end
       resources :merchants, only: [:index, :show]
 
