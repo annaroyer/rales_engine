bfcd7f76e9db0473b72282acd4c7b93a30d23d5c
katyjane8 <katy.jane8@gmail.com>
2018-03-14 17:16:00 -0600

    methods for BI on merchants started

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -4,11 +4,19 @@ class Merchant < ApplicationRecord
   has_many :invoice_items, through: :invoices
   validates_presence_of :name
 
-  def total_revenue
-    invoices.sum(:total_revenue)
+  def self.most_revenue(quantity)
+    select('merchants.*, sum(invoice_items.quantity * invoice_items.unit_price) as revenue')
+    .joins(:invoice_items)
+    .order('revenue DESC')
+    .group(:id)
+    .limit(quantity)
   end
 
-  def self.most_revenue(quantity)
-    order(:total_revenue).reverse_order.limit(quantity)
+  def self.most_items(quantity)
+    select('merchants.*, sum(invoice_items.quantity) as items_sold')
+    .joins(:invoice_items)
+    .group(:id)
+    .order('items_sold DESC')
+    .limit(quantity)
   end
 end
