7c6e9f0e0471cdc832a9c474df5c83116dfbb878
katyjane8 <katy.jane8@gmail.com>
2018-03-15 17:00:16 -0600

    most revenue, most items and total revenue

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -2,6 +2,8 @@ class Merchant < ApplicationRecord
   has_many :items
   has_many :invoices
   has_many :invoice_items, through: :invoices
+  has_many :customers, through: :invoices
+  has_many :transactions, through: :invoices
   validates_presence_of :name
 
   def self.most_revenue(quantity)
@@ -18,5 +20,11 @@ class Merchant < ApplicationRecord
     .group(:id)
     .order('items_sold DESC')
     .limit(quantity)
+  end
+
+  def self.total_revenue(date)
+    joins(:invoice_items, :invoices, :transactions)
+    .merge(Transaction.unscoped.success)
+    .sum("quantity * invoice_items.unit_price")
   end
 end
